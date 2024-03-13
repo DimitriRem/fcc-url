5b6c35f2259833d14523a154c54396b2b81c98e9
Dimitri Remoundos <dimitri.remoundos@gmail.com>
2024-03-13 13:15:07 +0000

    zz

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser')
+const dns = require('dns')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -28,29 +29,35 @@ let shortUrlList = [
   "nada",
 ];
 
-function checkUrl (string) {
-  let givenURL ;
-  try {
-      givenURL = new URL (string);
-  } catch (error) {
-      console.log ("error is", error);
-     return res.json({ error: 'invalid url' }); 
-  }
-  return true;
-}
-
+const options = { 
+  all:true, 
+}; 
 
 app.post('/api/shorturl', function(req, res) {  
-  let givenURL ;
+  let givenURL = "";
+  let isValid=false;  
   try {
     givenURL = new URL (req.body.url);
 } catch (error) {
   console.log ("error is", error);
  return res.json({ error: 'invalid url' }); 
 }
+  const domain = givenURL.hostname;
+  dns.lookup(domain, options, (err, address) =>
+  {
+    if (err) {
+      console.log(err);
+      return res.json({ error: 'invalid url' });
+    } else {
+      console.log("OK"+address);
+      isValid=true;
+    }
+  }
+  )
+if (isValid) {
   const shortcode=shortUrlList.length;
   shortUrlList.push(req.body.url);
-  res.json({original_url: req.body.url, short_url: shortcode})
+  res.json({original_url: req.body.url, short_url: shortcode})}
 })
 
 app.get('/api/shorturl/:arg?', function(req, res) {
