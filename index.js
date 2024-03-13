00d3f56bd7954b1f1ded21b8832930e7bbe91d8c
Dimitri Remoundos <dimitri.remoundos@gmail.com>
2024-03-13 13:44:52 +0000

    zxzx

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ const dns = require('dns')
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
@@ -29,38 +29,34 @@ let shortUrlList = [
   "nada",
 ];
 
-app.post('/api/shorturl', function(req, res) {  
+app.post('/api/shorturl', function (req, res) {
   let givenURL = "";
-  let isValid=true;  
+  let isValid = true;
   try {
-    givenURL = new URL (req.body.url);
-} catch (error) {
-  console.log ("01 error is", error);
- return res.json({ error: 'invalid url' }); 
-}
+    givenURL = new URL(req.body.url);
+  } catch (error) {
+    console.log("01 error is", error);
+    return res.json({ error: 'invalid url' });
+  }
   const domain = givenURL.hostname;
-  dns.lookup(domain, (err, url_Ip) =>
-  {
+  dns.lookup(domain, (err, url_Ip) => {
     if (err) {
-      console.log("02"+err);
+      console.log("02" + err);
       return res.json({ error: 'invalid url' });
     } else {
-      console.log("OK"+url_Ip);
-      isValid=true;
+      console.log("OK" + url_Ip);
+      const shortcode = shortUrlList.length;
+      shortUrlList.push(req.body.url);
+      res.json({ original_url: req.body.url, short_url: shortcode })
     }
   }
   )
-  console.log("03"+isValid);
-if (isValid) {
-  const shortcode=shortUrlList.length;
-  shortUrlList.push(req.body.url);
-  res.json({original_url: req.body.url, short_url: shortcode})}
 })
 
-app.get('/api/shorturl/:arg?', function(req, res) {
+app.get('/api/shorturl/:arg?', function (req, res) {
   res.redirect(shortUrlList[req.params.arg])
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
